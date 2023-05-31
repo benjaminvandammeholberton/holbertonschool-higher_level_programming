3b9331b328698dba29d79b0f33a4fb3033b43ad9
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-05-31 09:19:47 +0200

    Add: task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) < 123 and ord(i) > 96:
+            print(chr(ord(i) - 32), end="")
+        else:
+            print(i, end="")
+    print('')
