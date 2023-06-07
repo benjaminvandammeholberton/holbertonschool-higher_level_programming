ff9359ce5ad361616ccb1bc4a409c2ca60fd813b
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-07 10:41:43 +0200

    Fix: task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_value = 0
-
-    if a_dictionary is None:
-        return None
+    best_key = None
+    best_value = float('-inf')
 
     for key, value in a_dictionary.items():
-        if value > max_value:
-            max_value = value
-            max_key = key
-    return max_key
+        if isinstance(value, int) and value > best_value:
+            best_key = key
+            best_value = value
+
+    return best_key
+
