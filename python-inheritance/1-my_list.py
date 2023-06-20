3c6cafcbfa9144219d5e4c89345796c1cbe61c68
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-20 17:30:58 +0200

    Add: task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""1-my_list.py
+Test file: 1-my_list.txt
+Class: MyList
+Method(s): print_sorted
+To test this method, run:
+python3 -m doctest ./test/1-my_list.txt
+"""
 
-class Mylist(list):
-    """
-    A custom list class that inherits from the built-in list class.
-    """
-    pass
+
+class MyList(list):
+    """class that inherits from list."""
+    def print_sorted(self):
+        """prints the sorted list (ascending sort)."""
+        print(sorted(self))
