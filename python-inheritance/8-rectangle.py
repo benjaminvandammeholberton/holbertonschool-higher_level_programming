1462f222bec8f3ea6e12f52e3f7679a6e4195687
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-20 14:02:46 +0200

    Fix: task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -54,6 +54,8 @@ class Rectangle(BaseGeometry):
             TypeError: If width or height is not an integer.
             ValueError: If width or height is less than or equal to 0.
         """
+        self.__width = 0
+        self.__height = 0
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
