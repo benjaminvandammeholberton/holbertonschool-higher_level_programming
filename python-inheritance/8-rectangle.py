a3f2f595b4e59a1f32abdb04a3d9ad63d133024f
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-20 14:42:38 +0200

    Fix: task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,60 +1,35 @@
 #!/usr/bin/python3
-"""
-This script defines a class called 'BaseGeometry' with a method 'area' that
-raises an exception.
-"""
+
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """
-    This class serves as a base for defining geometric classes.
-    """
+    """Reprsent base geometry."""
+
     def area(self):
-        """
-        Raises an exception indicating that the 'area' method is not
-        implemented.
-        """
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates that the value is an integer greater than 0.
+        """Validate a parameter as an integer.
 
         Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
         Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
+    """Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
-        """
-        self.integer_validator("height", height)
-        self.integer_validator("width", width)
+        BaseGeometry.integer_validator(self, 'height', height)
         self.__height = height
+        BaseGeometry.integer_validator(self, 'width', width)
         self.__width = width
