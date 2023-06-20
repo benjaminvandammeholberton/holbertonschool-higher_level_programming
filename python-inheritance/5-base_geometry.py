e61f74d36a253d9da5ef272ef51970afcaa7bb9d
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-20 12:46:46 +0200

    Add: task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This script create an empty class 'BaseGeometry'
+"""
+
+
+class BaseGeometry:
+    """
+    It is used as a base to define specific geometric classes.
+    """
+    pass
