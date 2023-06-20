c67136514ebd209b1907ed0f4b6a078153d70957
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-20 17:04:08 +0200

    Fix: task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,33 +5,7 @@ raises an exception.
 """
 
 
-class BaseGeometry:
-    """
-    This class serves as a base for defining geometric classes.
-    """
-    def area(self):
-        """
-        Raises an exception indicating that the 'area' method is not
-        implemented.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that the value is an integer greater than 0.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
