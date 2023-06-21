04a74498e2a48d34f11bebbd58865e208e219f31
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-21 11:13:16 +0200

    Fix: task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines a function that prints the contents of a file
+"""
 
 def read_file(filename=""):
+    """
+    Reads and prints the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read. If not provided, an
+        empty string is used.
+
+    Returns:
+        None
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
