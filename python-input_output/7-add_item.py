40cdead5e507ac202fb7fa8235450c3d84244463
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-21 14:52:47 +0200

    Fix: task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
 """
+This script adds command-line arguments to a Python list and saves
+it to a JSON file.
 """
 import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """
+    Main function that adds command-line arguments to a Python
+    list and saves it to a JSON file.
+
+    Arguments:
+    - None
+
+    Returns:
+    - None
+
+    Side Effects:
+    - Appends command-line arguments to the data list.
+    - Saves the updated list to the JSON file.
+
+    Example:
+    >>> main()
+    ["Best", "School"]
+    """
+
 
 filename = "add_item.json"
 
