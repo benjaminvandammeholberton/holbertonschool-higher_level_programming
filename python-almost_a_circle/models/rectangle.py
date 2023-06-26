2c5dcde0a40a22249276716b87c558763dc4337c
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-26 17:52:27 +0200

    Add: task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -174,6 +174,13 @@ class Rectangle(Base):
             *args: Variable number of arguments containing the values to update
             the instance variables.
         """
-        variables = ['id', 'width', 'height', 'x', 'y']
-        for i, arg in enumerate(args):
-            setattr(self, variables[i], arg)
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
