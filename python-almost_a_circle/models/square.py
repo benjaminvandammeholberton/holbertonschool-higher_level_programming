5970decdd164f5b6705c42388f89143196f311c4
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-27 09:37:39 +0200

    Add: task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,16 +7,28 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-
+    This class represents a square, which is a special case of a rectangle
+    where all sides have equal length.
     """
     def __init__(self, size, x=0, y=0, id=None):
         """
+        Initializes a Square instance.
 
+        Args:
+            size (int): The length of the square's sides.
+            x (int, optional): The x-coordinate of the square's position.
+            Defaults to 0.
+            y (int, optional): The y-coordinate of the square's position.
+            Defaults to 0.
+            id (int, optional): The identifier of the square. Defaults to None.
         """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """
+        Returns a string representation of the Square.
 
+        Returns:
+            str: A string representation of the Square object.
         """
         return f"[Square] ({self.id}), {self.x}/{self.y} - {self.width}"
