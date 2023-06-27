3fc25fa949d49bf46f1a465dec6593fa635f2e84
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-06-27 15:46:10 +0200

    Add: task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,6 @@ class Base:
     It serves as the base class for other classes in the module.
     """
     __nb_objects = 0
-    list_dictionaries = []
 
     def __init__(self, id=None):
         """
@@ -26,22 +25,14 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
-        self.list_dictionaries.append(self.to_dictionary())
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Converts a list of dictionaries to a JSON-formatted string.
+        """Return the JSON serialization of a list of dicts.
 
         Args:
             list_dictionaries (list): A list of dictionaries.
-
-        Returns:
-            str: A JSON-formatted string representation of the list of
-            dictionaries.
-                If the input list is None or empty, returns "[]".
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
