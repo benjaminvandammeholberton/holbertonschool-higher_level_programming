40f2691d3384f834482ece6f1803aec8cbdead72
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-24 23:40:54 +0200

    Fix: task 17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,5 @@
--- script that converts a database to UTF8
+-- This script converts hbtn_0c_0 database to UTF8.
+-- Uses hbtn_0c_0.
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
