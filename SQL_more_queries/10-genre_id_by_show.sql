abfb24484bb9b9d4abac7acedb06d651c36c62e0
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-28 21:48:50 +0200

    FIX: task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
-select * from tv_show_genres;
-
-select * from tv_shows;
-
 SELECT
     tv_shows.title,
     tv_show_genres.genre_id
