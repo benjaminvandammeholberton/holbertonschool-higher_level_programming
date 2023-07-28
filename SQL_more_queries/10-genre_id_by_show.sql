d72c6be8d113539fc249881284115b137b949e70
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-28 21:47:30 +0200

    FIX: task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -7,7 +7,7 @@ select * from tv_shows;
 SELECT
     tv_shows.title,
     tv_show_genres.genre_id
-from tv_shows
+FROM tv_shows
     JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY
     tv_shows.title ASC,
