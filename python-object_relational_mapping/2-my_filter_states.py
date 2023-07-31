a6421a1baf2d2661a4e248a9a45f625a4188f171
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-31 11:55:40 -0700

    Add: task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,8 @@ if __name__ == '__main__':
     name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row == sys.argv[4]:
+            print(row)
 
     cur.close()
     db.close()
