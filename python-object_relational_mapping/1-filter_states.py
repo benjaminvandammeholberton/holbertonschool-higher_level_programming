6060b07aa63ae6ced8c025d28265e005c0c5944a
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-31 08:40:44 -0700

    Add: task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module that lists all states with a name\
+starting with N from the database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    for row in rows:
+        print(row)
