6d997049d07aaa95767fb4ea5ea51a354830932f
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-31 07:37:53 -0700

    Fix: task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,60 +3,59 @@ import MySQLdb
 import sys
 """
 This module connects to a MySQL database
-and retrieves data from the 'states'table.
+and retrieves data from the 'states' table.
 """
 
+if __name__ = '__main__':
 
-def main():
-    """
-    Connects to a MySQL database and retrieves data from the 'states' table.
-    It searches for states whose name starts with 'N' and prints the results.
+    def main():
+        """
+        Connects to a MySQL database and retrieves\
+        data from the 'states' table.
+        It searches for states whose name starts\
+        with 'N' and prints the results.
 
-    MySQL Database Connection Details:
+        MySQL Database Connection Details:
         - Host: localhost
         - User: root
         - Password: (empty)
         - Database: hbtn_0e_0_usa
         - Port: 3306
 
-    Note: Make sure to install the 'MySQLdb'
-    module using 'pip install MySQL-python'.
+        Note: Make sure to install the 'MySQLdb'
+        module using 'pip install MySQL-python'.
 
-    Example Usage:
+        Example Usage:
         $ python3 your_script.py
-    """
-    MY_HOST = "localhost"
-    MY_USER = sys.argv[1]
-    MY_PASSWD = sys.argv[2]
-    MY_DB = sys.argv[3]
-    MY_PORT = 3306
+        """
+        MY_HOST = "localhost"
+        MY_USER = sys.argv[1]
+        MY_PASSWD = sys.argv[2]
+        MY_DB = sys.argv[3]
+        MY_PORT = 3306
 
-    try:
-        db = MySQLdb.connect(
-            host=MY_HOST,
-            user=MY_USER,
-            passwd=MY_PASSWD,
-            db=MY_DB,
-            port=MY_PORT,
-        )
-        cur = db.cursor()
-
-        cur.execute("SELECT * FROM states WHERE name \
-                    LIKE 'N%' ORDER BY id ASC")
-        rows = cur.fetchall()
-    except MySQLdb.Error as e:
         try:
-            print(f"MySQL Error {e.args[0]}: {e.args[1]}")
-        except IndexError:
-            print(f"MySQL Error: {str(e)}")
+            db = MySQLdb.connect(
+                host=MY_HOST,
+                user=MY_USER,
+                passwd=MY_PASSWD,
+                db=MY_DB,
+                port=MY_PORT,
+            )
+            cur = db.cursor()
 
-        finally:
-            cur.close()
-            db.close()
+            cur.execute("SELECT * FROM states WHERE name \
+            LIKE 'N%' ORDER BY id ASC")
+            rows = cur.fetchall()
+        except MySQLdb.Error as e:
+            try:
+                print(f"MySQL Error {e.args[0]}: {e.args[1]}")
+            except IndexError:
+                print(f"MySQL Error: {str(e)}")
 
-    for row in rows:
-        print(row)
+            finally:
+                cur.close()
+                db.close()
 
-
-if __name__ == '__main__':
-    main()
+                for row in rows:
+                    print(row)
