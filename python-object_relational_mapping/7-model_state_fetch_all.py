3d4851a822afd65a974b9370af1af73c6ebad093
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-01 04:50:56 -0700

    Fix: task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,6 +15,7 @@ if __name__ = '__main__':
                           sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
     session = Session()
 
     states = session.query(State).all()
