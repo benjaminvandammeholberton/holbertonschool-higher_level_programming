097d9a0f9c8ee7baa8fb027572f488db1bd0f59b
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-01 07:39:57 -0700

    Fix: task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,15 +10,15 @@ if __name__ == '__main__':
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
-    @localhost/{sys.argv[3]}", pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    states_with_a = session.query(State).filter(State.name.contains(
+    Session.configure(bind=engine)
+    s = Session()
+    a = s.query(State).filter(State.name.contains(
         'a')).order_by(State.id).all()
-    print((states_with_a))
-    for state_with_a in states_with_a:
-        print(f"{state_with_a.id}: {state_with_a.name}")
-
-    session.close()
+    for instance in a:
+        print("{}: {}".format(instance.id, instance.name))
+    s.close()
