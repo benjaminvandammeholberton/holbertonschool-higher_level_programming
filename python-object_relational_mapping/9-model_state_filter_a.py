20d5fa94113fc7600e8842f8816f975472fa98e4
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-01 07:36:17 -0700

    Fix: task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_with_a = session.query(State).filter(State.name.like('%a%'))\
-                                        .order_by(State.id).all()
+    states_with_a = session.query(State).filter(State.name.contains(
+        'a')).order_by(State.id).all()
     print((states_with_a))
     for state_with_a in states_with_a:
         print(f"{state_with_a.id}: {state_with_a.name}")
