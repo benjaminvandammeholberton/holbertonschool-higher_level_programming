d23f47f24b548fc60e65b9c7575dbf9e836ad895
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-01 04:42:40 -0700

    Fix: task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,14 +2,13 @@
 """
 script that lists all 'State' objects from the database 'hbtn_0e_6_usa'
 """
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
 
 if __name__ = '__main__':
-
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
-    import sys
-
+    
     engine = create_engine('mysql+mysqldb://{}:{}\
     @localhost/{}'.format(sys.argv[1], sys.argv[2],
                           sys.argv[3]), pool_pre_ping=True)
@@ -19,3 +18,5 @@ if __name__ = '__main__':
     states = session.query(State).all()
     for state in states:
         print(f"{state.id}: {state.name}")
+
+    
