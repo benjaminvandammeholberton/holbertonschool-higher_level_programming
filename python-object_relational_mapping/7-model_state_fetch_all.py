372d065afb6a26f5385b2202d820f0693a477c50
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-01 04:55:04 -0700

    Fix: task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa"""
 
-import sqlalchemy as sa
+import sqlalchemy
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
