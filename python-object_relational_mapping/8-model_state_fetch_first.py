d7276062ae20ad42f20cd51591c566325004d988
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-08-01 07:02:07 -0700

    Add: task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!//usr/bin/python3
+"""
+Module that  prints the first State object from the database hbtn_0e_6_usa
+"""
+
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
+    @localhost/{sys.argv[3]}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first = session.query(State).first()
+    try:
+        print(f"{first.id}: {first.name}")
+    except Exception:
+        print("Nothing")
+    session.close()
